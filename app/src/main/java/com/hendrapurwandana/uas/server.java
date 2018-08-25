249ad98418b1a50d42ee3b6263c4783d28ebf482
hendrapurwandana <hendrapurwandana14@gmail.com>
2018-08-25 22:26:18 +0700

    v-01

---
+++ app/src/main/java/com/hendrapurwandana/uas/server.java
@@ -0,0 +1,9 @@
+package com.hendrapurwandana.uas;
+
+/**
+ * Created by hendra purwandana on 12/08/2018.
+ */
+
+public class server {
+    public static final String URL = "http://sasmitoh.nitarahmawati.my.id/";
+}
